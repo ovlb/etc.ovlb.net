a6d9354fdd6337f02e39a3829716ec6fd16caa04
Oscar <o@ovlb.net>
2016-08-22 17:21:37 +0200

    add article route

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
-
-const MongoClient = require('mongodb').MongoClient;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon in /dist
 app.use(favicon(path.join(__dirname, 'dist/img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -25,8 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 app.listen(4000, () => {
-  console.log('listening on 4000')
+  console.log('listening on 4000');
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const fs = require('fs');
+const readFile = fs.readFile;
+var md = require('markdown');
+
+var readFilePromise = function(filename) {
+  return new Promise(
+    function(resolve, reject) {
+      readFile(filename, 'utf8', (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.locals.title = 'Start';
+  res.render('index');
+});
+
+router.get('/:artikel', (req, res, next) => {
+  var dataObject = [];
+  const artikel = req.params.artikel;
+
+  const data = artikel + '/data.json';
+  const text = artikel + '/text.md';
+
+  const pathToData = path.join(process.cwd(), 'source/artikel', data);
+  const pathToText = path.join(process.cwd(), 'source/artikel', text);
+
+  var textContent = fs.readFileSync(pathToText, 'utf8');
+
+  readFilePromise(pathToData)
+  .then(txt => {
+    var parsedText = JSON.parse(txt);
+    res.locals.title = parsedText.title;
+    res.locals.article = parsedText;
+    res.locals.text = textContent;
+    res.locals.md = md;
+    console.log(res.locals);
+    res.render('article');
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
 
 module.exports = router;
