45891778fe548e545f17796a7fd3ef202e5cf55f
Oscar <o@ovlb.net>
2016-08-23 18:57:47 +0200

    don’t delete fonts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -102,7 +102,7 @@ gulp.task('scripts', () =>
 
 
 // Clean output directory
-gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git'], {dot: true}));
+gulp.task('clean', () => del(['.tmp', 'dist/*', '!dist/.git', '!dist/fonts'], {dot: true}));
 
 // Watch files for changes & reload
 gulp.task('serve', ['scripts', 'styles', 'copy', 'nodemon'], () => {
