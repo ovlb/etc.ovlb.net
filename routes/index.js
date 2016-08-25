feb510237a4f92baf6171071fc401f091035fe8a
Oscar <o@ovlb.net>
2016-08-25 10:21:17 +0200

    readFiles for list of articles, put locals in object

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 const readFile = fs.readFile;
+const dir = require('node-dir');
 var md = require('markdown');
 
 var readFilePromise = function(filename) {
@@ -20,30 +21,68 @@ var readFilePromise = function(filename) {
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.locals.title = 'Start';
-  res.render('index');
+  const artikelPath = path.join(process.cwd(), 'source/artikel/');
+  var artikelMeta = [];
+
+  /**
+   * Read the contents of JSON files in the artikel folder. These contain the
+   * metadata of the respective articles
+   *
+   * Sort them by date
+   *
+   * Render index template
+   */
+  dir.readFiles(artikelPath, {
+    match: /.json$/
+  }, (err, content, next) => {
+    if (err) throw err;
+    var parsed = JSON.parse(content);
+    artikelMeta.push(parsed);
+    // Index should show the articles in reversed chronological order
+    artikelMeta.sort( (a, b) => {
+      var dateA = new Date(a.sortDate);
+      var dateB = new Date(b.sortDate);
+      return dateB - dateA;
+    });
+    next();
+  }, (err, files) => {
+    if (err) throw err;
+    res.render('index', {
+      title: 'Start',
+      artikel: artikelMeta
+    });
+  })
 });
 
+/**
+ * GET single article
+ */
 router.get('/:artikel', (req, res, next) => {
   var dataObject = [];
   const artikel = req.params.artikel;
 
+  // Construct paths to metadata and the Markdown files containing the text
   const data = artikel + '/data.json';
   const text = artikel + '/text.md';
-
   const pathToData = path.join(process.cwd(), 'source/artikel', data);
   const pathToText = path.join(process.cwd(), 'source/artikel', text);
 
   var textContent = fs.readFileSync(pathToText, 'utf8');
 
+  /**
+   * Parse the metadata, include the text and render the index page
+   */
   readFilePromise(pathToData)
   .then(txt => {
-    var parsedText = JSON.parse(txt);
-    res.locals.title = parsedText.title;
-    res.locals.article = parsedText;
-    res.locals.text = textContent;
-    res.locals.md = md;
-    res.render('article');
+    var parsedMeta = JSON.parse(txt);
+    const locals = {
+      title: parsedMeta.title,
+      article: parsedMeta,
+      text: textContent,
+      md: md
+    };
+
+    res.render('article', locals);
   })
   .catch(error => {
     console.log(error);
