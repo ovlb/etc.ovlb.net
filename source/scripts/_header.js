ba777286e5cd91ddc66d3d5092f807abcc833f5f
Oscar <o@ovlb.net>
2016-08-26 21:44:46 +0200

    rename main.js -> _header.js

---
+++ source/scripts/_header.js
@@ -0,0 +1,33 @@
+const headerEl = document.querySelector('.js-main-header');
+const contentEl = document.querySelector('.js-main-content');
+
+var activateEventListeners = () => {
+  document.addEventListener('scroll', debounceChangeHeader);
+};
+
+
+/**
+ * Controls the header on scroll
+ */
+var controlHeader = () => {
+  const cssClass = 'o-main-header--scrolled';
+  var offsetToScroll = contentEl.offsetTop;
+  var scrollOffsetY = window.pageYOffset ||
+    document.documentElement.scrollTop;
+  var headerHeight = headerEl.clientHeight;
+
+  if (scrollOffsetY > offsetToScroll && !headerEl.classList.contains(cssClass)) {
+    headerEl.classList.add(cssClass);
+  } else if (scrollOffsetY < offsetToScroll && headerEl.classList.contains(cssClass)) {
+    headerEl.classList.remove(cssClass);
+  }
+}
+
+var debounceChangeHeader =  debounce(function(){
+  controlHeader();
+}, 40);
+
+(() => {
+  console.log('lol');
+  activateEventListeners();
+})();
