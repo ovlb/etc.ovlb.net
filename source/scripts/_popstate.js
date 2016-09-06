be05d8163393e9d8ef260bbcda850ed6cf502c6c
Oscar <o@ovlb.net>
2016-09-06 20:25:16 +0200

    render article w/ dom methods

--- source/scripts/_popstate.js
+++ source/scripts/_popstate.js
@@ -8,19 +8,20 @@ if ( contentEl.classList.contains(articleClass) ) {
   const articles = document.querySelectorAll('js-article');
 
   contentEl.addEventListener('click', function(event) {
-    if ( event.target != event.currentTarget) {
+    if ( event.target != event.currentTarget ) {
       event.preventDefault();
       // parentNode as the event target is the link, not the box itself
       const slug = event.target.parentNode.getAttribute('data-slug');
 
+      // AJAX request to get the article data
       get(slug).then( (response) => {
-        const data = response;
+        const data = JSON.parse(response);
         console.log(response);
         history.pushState(data, null, slug);
-        renderPage(data);
+        renderArticle(data);
         contentEl.classList.remove(indexClass);
         contentEl.classList.remove('o-archive');
-        document.title = response.title + ' | etc.ovlb';
+        document.title = data.title + ' | etc.ovlb';
         contentEl.classList.add(articleClass);
       }, (error) => {
         console.log('Error: ' + response);
@@ -30,6 +31,37 @@ if ( contentEl.classList.contains(articleClass) ) {
   }, false);
 }
 
-var renderPage = (data) => {
-  contentEl.innerHTML = data;
+var renderArticle = (data) => {
+  let newArticleHeaderEl = document.createElement('header');
+  newArticleHeaderEl.classList.add('o-article-header');
+  let newArticleMetaEl = document.createElement('p');
+
+  let articleType = data.type;
+  let articleDate = data.date;
+  let articleMeta = document.createTextNode(articleType + '—' + articleDate);
+  newArticleMetaEl.classList.add('c-card__meta', 'p--small');
+  newArticleMetaEl.appendChild(articleMeta);
+  newArticleHeaderEl.appendChild(newArticleMetaEl);
+
+  let newArticleHeadingEl = document.createElement('h1');
+  newArticleHeadingEl.classList.add('c-main-headline');
+  let newArticleHeading = document.createTextNode(data.title);
+  newArticleHeadingEl.appendChild(newArticleHeading);
+  newArticleHeaderEl.appendChild(newArticleHeadingEl);
+
+  if(data.subtitle) {
+    let newArticleSubHeadingEl = document.createElement('h2');
+    newArticleSubHeadingEl.classList.add('c-sub-headline');
+    let newArticleSubHeading = document.createTextNode(data.subtitle);
+    newArticleSubHeadingEl.appendChild(newArticleSubHeading);
+    newArticleHeaderEl.appendChild(newArticleSubHeadingEl);
+  }
+
+  let newArticleContentEl = document.createElement('section');
+  newArticleContentEl.classList.add('o-article');
+  newArticleContentEl.innerHTML = data.text;
+
+  contentEl.innerHTML = '';
+  contentEl.appendChild(newArticleHeaderEl);
+  contentEl.appendChild(newArticleContentEl);
 }
